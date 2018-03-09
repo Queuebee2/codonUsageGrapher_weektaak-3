d39ecc1ba20fadfd759d1315e20fe7425cbe4a8c
Milain Lambers <32010930+Queuebee2@users.noreply.github.com>
2018-03-09 05:31:00 +0100

    some extra characters
    always look on the dark side:
    - scrolling window algorithm unreliable/no verification method (yet)
    - no consideration for other than {'A','T','G','C','N'} characters (yet)
    - graphing function not tested (YET)
    - weird output like 'TOTAL' being thousands of times too low as if it doesn't count up somewhere.
    - messy code (still)
    - unreadable

--- CUG/codonUsageGrapher.py
+++ CUG/codonUsageGrapher.py
@@ -15,9 +15,9 @@ version = "0.5"
 # changelog
 
 #NEW
-# 
-# 6-3-2018
-# 
+# and codon2Amino to translate
+# added dictionaries 'emptyData' to keep track of self.codonUsage
+#? 
 # pasted dictionaries from  blok2, afvink3 (temporarily?) (U->T)
 # added function:   createFastaobjects
 # added function:   getFilenames
@@ -34,48 +34,123 @@ import os
 from pathlib import Path
 
 
-#hardcoded
+#hardcoded globals
 # delimiter = '_'
-# 
+#
+pathKeyword = 'sequences'
+recursionDepth = 3
+specialFilenames = ['arabidopsis-thaliana_chr2.fasta',
+                    'escherichia-coli_genome.fasta',
+                    'salmonella-enterica_genome.fasta',
+                    'homo-sapiens_chr12.fasta']
+                    
 
-def main(verbose=False):
-
-    # yes, it's hardcoded. 
-    sequencesPath = findUp('sequences')
+def main(verbosity=False):
+    """
+    """
+    # find fasta files
+    sequencesPath = findUp(pathKeyword)
     print("Found presumable path with sequences")
     print(sequencesPath)
     spacer()
     setCWD(sequencesPath)
-    
     availableFastas =  getFilenames()
-    fastaObjects = createFastaobjects(availableFastas, 'CDS')
-    
 
-aa3 = {"Ala ": ["GCT" , "GCC" , "GCA" , "GCG"] ,
-       "Arg": ["CGT" , "CGC" , "CGA" , "CGG" , "AGA" , "AGG"] ,
-       "Asn": ["AAT" , "AAC"] ,
-       "Asp": ["GAT" , "GAC"] ,
-       "Cys": ["TGT" , "TGC"] ,
-       "Gln": ["CAA" , "CAG"] ,
-       "GlT": ["GAA" , "GAG"] ,
-       "Gly": ["GGT" , "GGC" , "GGA" , "GGG"] ,
-       "His": ["CAT" , "CAC"] ,
-       "Ile": ["ATT" , "ATC" , "ATA"] ,
-       "LeT": ["TTA" , "TTG" , "CTT" , "CTC" , "CTA" , "CTG"] ,
-       "Lys": ["AAA" , "AAG"] ,
-       "Met": ["ATG"] ,
-       "Phe": ["TTT" , "TTC"] ,
-       "Pro": ["CCT" , "CCC" , "CCA" , "CCG"] ,
-       "Ser": ["TCT" , "TCC" , "TCA" , "TCG" , "AGT" ,"AGC"] ,
-       "Thr": ["ACT" , "ACC" , "ACA" , "ACG"] ,
-       "Trp": ["TGG"] ,
-       "Tyr": ["TAT" , "TAC"] ,
-       "Val": ["GTT" , "GTC" , "GTA" , "GTG"] ,
-       "Start": ["ATG" , "CTG" , "TTG" , "GTG" , "ATT"] ,
-       "Stop" : ["TAG" , "TGA" , "TAA"]}
+    # create objects to hold information
+    objectsToGraph = createFastaObjects(availableFastas, 'CDS', specialFilenames)
+
+    if verbosity:
+        print("printing object attributes")
+        for o in objectsToGraph:
+            print('printing name:',o.name)
+            o.printSummary()
+            o.createDictionary()
+
+
+    print("hopefully I did that right...")
+
+# lots of errors because I realised this too late
+"""
+A = adenine
+C = cytosine
+G = guanine
+T = thymine
+R = G A (purine)
+Y = T C (pyrimidine)
+K = G T (keto)
+M = A C (amino)
+S = G C (strong bonds)
+W = A T (weak bonds)
+B = G T C (all but A)
+D = G A T (all but C)
+H = A C T (all but G)
+V = G C A (all but T)
+N = A G C T (any)
+"""
+
+emptyCodonUsage = {'TOTAL': 0,
+                   'ERRORS':0,
+ 'Ala ': {'Ala _TOTAL': 0, 'GCT': 0, 'GCC': 0, 'GCA': 0, 'GCG': 0},
+ 'Arg': {'Arg_TOTAL': 0, 'CGT': 0, 'CGC': 0, 'CGA': 0, 'CGG': 0, 'AGA': 0, 'AGG': 0},
+ 'Asn': {'Asn_TOTAL': 0, 'AAT': 0, 'AAC': 0},
+ 'Asp': {'Asp_TOTAL': 0, 'GAT': 0, 'GAC': 0},
+ 'Cys': {'Cys_TOTAL': 0, 'TGT': 0, 'TGC': 0},
+ 'Gln': {'Gln_TOTAL': 0, 'CAA': 0, 'CAG': 0},
+ 'GlT': {'GlT_TOTAL': 0, 'GAA': 0, 'GAG': 0},
+ 'Gly': {'Gly_TOTAL': 0, 'GGT': 0, 'GGC': 0, 'GGA': 0, 'GGG': 0},
+ 'His': {'His_TOTAL': 0, 'CAT': 0, 'CAC': 0},
+ 'Ile': {'Ile_TOTAL': 0, 'ATT': 0, 'ATC': 0, 'ATA': 0},
+ 'LeT': {'LeT_TOTAL': 0, 'TTA': 0, 'TTG': 0, 'CTT': 0, 'CTC': 0, 'CTA': 0, 'CTG': 0},
+ 'Lys': {'Lys_TOTAL': 0, 'AAA': 0, 'AAG': 0},
+ 'Met': {'Met_TOTAL': 0, 'ATG': 0},
+ 'Phe': {'Phe_TOTAL': 0, 'TTT': 0, 'TTC': 0},
+ 'Pro': {'Pro_TOTAL': 0, 'CCT': 0, 'CCC': 0, 'CCA': 0, 'CCG': 0},
+ 'Ser': {'Ser_TOTAL': 0, 'TCT': 0, 'TCC': 0, 'TCA': 0, 'TCG': 0, 'AGT': 0, 'AGC': 0},
+ 'Thr': {'Thr_TOTAL': 0, 'ACT': 0, 'ACC': 0, 'ACA': 0, 'ACG': 0},
+ 'Trp': {'Trp_TOTAL': 0, 'TGG': 0},
+ 'Tyr': {'Tyr_TOTAL': 0, 'TAT': 0, 'TAC': 0},
+ 'Val': {'Val_TOTAL': 0, 'GTT': 0, 'GTC': 0, 'GTA': 0, 'GTG': 0},
+ 'Start': {'Start_TOTAL': 0, 'ATG': 0, 'CTG': 0, 'TTG': 0, 'GTG': 0, 'ATT': 0},
+ 'Stop': {'Stop_TOTAL': 0, 'TAG': 0, 'TGA': 0, 'TAA': 0}}
+    
+codon2Amino = {'GCT': 'Ala ', 'GCC': 'Ala ',
+               'GCA': 'Ala ', 'GCG': 'Ala ',
+               'CGT': 'Arg', 'CGC': 'Arg', 'CGA': 'Arg',
+               'CGG': 'Arg', 'AGA': 'Arg', 'AGG': 'Arg',
+               'AAT': 'Asn', 'AAC': 'Asn', 'GAT': 'Asp',
+               'GAC': 'Asp', 'TGT': 'Cys', 'TGC': 'Cys',
+               'CAA': 'Gln', 'CAG': 'Gln', 'GAA': 'GlT',
+               'GAG': 'GlT', 'GGT': 'Gly', 'GGC': 'Gly',
+               'GGA': 'Gly', 'GGG': 'Gly', 'CAT': 'His',
+               'CAC': 'His', 'ATT': 'Ile', 'ATC': 'Ile',
+               'ATA': 'Ile', 'TTA': 'LeT', 'TTG': 'LeT',
+               'CTT': 'LeT', 'CTC': 'LeT', 'CTA': 'LeT',
+               'CTG': 'LeT', 'AAA': 'Lys',
+               'AAG': 'Lys', 'ATG': 'Met',
+               'TTT': 'Phe', 'TTC': 'Phe',
+               'CCT': 'Pro', 'CCC': 'Pro',
+               'CCA': 'Pro', 'CCG': 'Pro',
+               'TCT': 'Ser', 'TCC': 'Ser',
+               'TCA': 'Ser', 'TCG': 'Ser',
+               'AGT': 'Ser', 'AGC': 'Ser',
+               'ACT': 'Thr', 'ACC': 'Thr',
+               'ACA': 'Thr', 'ACG': 'Thr',
+               'TGG': 'Trp', 'TAT': 'Tyr',
+               'TAC': 'Tyr', 'GTT': 'Val',
+               'GTC': 'Val', 'GTA': 'Val',
+               'GTG': 'Val',
+               'TAG': 'Stop', 'TGA': 'Stop', 'TAA': 'Stop'}
+
 
         
-def createFastaobjects(filesToParse, keyword=None, delimiter='_' ,verbose=False,):
+def createFastaObjects(filesToParse, keyword=None, special=None,delimiter='_' ,verbose=False,):
+    """
+    args
+        keyword: a keyword that has to be in the name
+
+        special: a list of strings containing exact names (for hardcoding purposes
+        if parsing is too complicated)
+    """
     createdStuff = [] # list of objects to return
     
     for fastaFilename in filesToParse:
@@ -84,12 +159,17 @@ def createFastaobjects(filesToParse, keyword=None, delimiter='_' ,verbose=False,
         description = parts[1]
         if not keyword:
             print('creating Fastafile object with name:',name,'\tdesc:',description)
-            fastaObject = Fastafile(name, description)
+            fastaObject = Fastafile(name, description, fastaFilename)
             createdStuff.append(fastaObject)
-        elif keyword in name or keyword in description:
+        elif keyword in fastaFilename:
             print('creating Fastafile object with name:',name,'\tdesc:',description)
-            fastaObject = Fastafile(name, description)
+            fastaObject = Fastafile(name, description, fastaFilename)
             createdStuff.append(fastaObject)
+        elif special:
+            if fastaFilename in special:
+                print('creating Fastafile object with name:',name,'\tdesc:',description)
+                fastaObject = Fastafile(name, description, fastaFilename)
+                createdStuff.append(fastaObject)
         else:
             if verbose: print("no keyword match with filename, no object.")
         
@@ -107,10 +187,10 @@ def spacer():
 
 def setCWD(path):
     os.chdir(path)
-    print("working in\"", os.getcwd(), "\"now")
+    print("working in \"", os.getcwd(), "\" now")
     spacer()
     
-def findUp(searchterm,thispath=False, depth=3,verbose=False):
+def findUp(searchterm, thispath=False, depth=recursionDepth ,verbose=False):
     """version 3, march 5, 2018
     changelog
     
@@ -137,8 +217,7 @@ def findUp(searchterm,thispath=False, depth=3,verbose=False):
 
     # end recursion
     if depth <= 0:
-        return "ran out of depth to go through"
-    
+        raise RecursionError("recusion limit (set to "+str(recursionDepth)+") exceeded")
     print('looking for',searchterm,'in',thispath)
     for dirname, nested_dirnames, absolute_filenames in os.walk(thispath):
         if dirname.split(oschar)[-1] == searchterm:
@@ -148,19 +227,166 @@ def findUp(searchterm,thispath=False, depth=3,verbose=False):
             return result
         
     if verbose: print('findUp going up a directory!')
-    return findUp(str(Path(thispath).parent), searchterm, depth-1)
+    return findUp(searchterm, str(Path(thispath).parent), depth-1)
 
 
 class Fastafile():
     """ docstring """
-    def __init__(self, name, desc):
+    def __init__(self, name, description, filename):
         self.name = name
-        self.desc = desc
-        self.usedCodon
+        self.description = description
+        self.filename = filename
+        self.codonUsage = emptyCodonUsage.copy()
+
+    def getName(self):
+        return self.name
+
+    def getDescription(self):
+        return self.name
+
+    def getFilename(self):
+        return self.filename
+        
+    def printSummary(self):
+        print('name:',self.name,'\tdesc:',self.description, '\thasfilename:', str(bool(self.filename)))
 
 
+    def createDictionary(self, startCodons=False, stopCodons=False, verbose=False):
+        """finds the start of a coding frame by using
+        the sliding window algorithm until it finds the index
+        of the first start codon and then creates a dictionary with
+        total:count and aminoacid:{total:count,codonsynonym:count} key-value pairs
 
+        changelog
 
+        args
+
+            sequenceFile: iOtextwrapper object of a fasta format
+            DNA coding sequence.
+
+            startcodons(default=False) bool/list: list of used
+            codons to filter by for the start of the frame.
+                False: uses all known startcodons
+
+            stopcodons(default=False) bool/list: list of used
+            codons to filter by for finding the end of the frame.
+                False: uses all known startcodons
+
+        returns
+
+        errors
+        
+        """    
+        # set defaults if none given
+        if not startCodons:
+            startCodons = ["ATG" , "CTG" , "TTG" , "GTG" , "ATT"]
+        if not stopCodons:
+            stopCodons = ["TAG" , "TGA" , "TAA"]
+
+        file = open(self.filename, 'r')
+
+        def findStart(string):
+            first = -1
+            for codon in startCodons:
+                index = string.find(codon)
+                print(codon, first, index)
+                if index < first and (index != -1):
+                    first = index
+                elif (first == -1) and (index > -1):
+                    first = index
+
+            print('smallest index', first)
+            return first
+
+        def isStopCodon(codon):
+            if codon in stopCodons:
+                print("stopcodon found", codon)
+                return True
+      
+
+        self.codonUsage = emptyCodonUsage.copy()
+
+        headersCounted = 0
+        framesCounted = 0
+        prevline = next(file)
+        frameFound = False
+
+        # no clue if the frame is correct at this point.
+        for line in file:
+            
+            line = line.strip() # remove '\n'
+            
+            if verbose: print(line)
+
+            # find a Frame
+            if not frameFound:
+                if prevline.startswith('>'):
+                    headersCounted+=1
+                    print("found header #"+str(headersCounted))
+                    prevline=line
+                else:
+                    window = prevline+line[:2]
+                    frameStart = findStart(window)
+                    if frameStart > -1:
+                        startCodon = window[frameStart] + window[frameStart+1] + window[frameStart+2]
+                        framesCounted +=1
+                        print('found frame #'+str(framesCounted)+' with frameStart matching',startCodon,'at',frameStart)
+                        frameshift = frameStart % 3
+                        frameFound = True
+                    # finish this by starting to count
+                    for n in range(frameStart, len(window)-2, 3):
+                        codon = codon = window[n]+window[n+1]+window[n+2]
+                        if isStopCodon(codon):
+                            frameFound = False
+                        else:
+                            try:
+                                if 'N' in codon:
+                                    self.codonUsage['TOTAL'] += 1
+                                else:
+                                    if verbose: print(codon)
+                                    aminoacid = codon2Amino[codon]
+                                    if verbose: print(aminoacid)
+                                    self.codonUsage[aminoacid][aminoacid+'_TOTAL'] += 1
+                                    self.codonUsage[aminoacid][codon] += 1
+                                    self.codonUsage['TOTAL'] += 1
+                            except KeyError:
+                                self.codonUsage['TOTAL']+=1
+                                self.codonUsage['ERRORS']+=1
+                                print(codon,'gave error')
+                            
+                            
+                        if verbose: print('vprint codon1:', codon, n)
+                    prevline = line
+                    
+            # if frame found
+            elif frameFound:
+                if not line.startswith('>'):
+                    window = prevline+line[:2]
+                    for i in range(0+frameshift, len(window)-2,3):
+                        codon = window[i]+window[i+1]+window[i+2]
+                        if verbose: print('vprint codon2:',codon, i)
+                        try:
+                            if 'N' in codon:
+                                self.codonUsage['TOTAL'] += 1
+                            else:
+                                if verbose: print(codon)
+                                aminoacid = codon2Amino[codon]
+                                if verbose: print(aminoacid)
+                                self.codonUsage[aminoacid][aminoacid+'_TOTAL'] += 1
+                                self.codonUsage[aminoacid][codon] += 1
+                                self.codonUsage['TOTAL'] += 1
+                        except KeyError:
+                            self.codonUsage['ERRORS']+=1
+                            self.codonUsage['TOTAL']+=1
+                            print(codon,'gave error')
+                    prevline = line
+                
+                else:
+                    frameFound = False
+                    prevline = line
+
+        print("codon usage for",self.filename)     
+        print(self.codonUsage)
 
 
 
@@ -168,4 +394,4 @@ class Fastafile():
 if __name__ == '__main__':
     print("running from main", version)
     print("-------------------------")
-    main()
+    main(True) # developer mode: verbose is turned on
